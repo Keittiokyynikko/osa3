f66a01e74fb3896e65fef99f8e856bfbb164cd82
Keittiokyynikko <karvinen.juuso@gmail.com>
2021-02-19 16:38:49 +0200

    muokkaus

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+const requestLogger = (req, res, next) => {
+    console.log('Method:', req.method)
+    console.log('Path:', req.path)
+    console.log('Body:', req.body)
+    console.log('---')
+    next()
+  }
 
 app.use(express.json())
+app.use(requestLogger)
 
 let persons = [
     {
@@ -68,16 +76,35 @@ app.post('/api/persons', (req, res) => {
     const body = req.body
     console.log(body)
 
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: generateId()
+    if(!body.name) {
+        return res.status(400).json({
+            error: 'name missing'
+        })
+    } else if(!body.number) {
+        return res.status(400).json({
+            error: 'number missing'
+        })
+    } else if(persons.find(n => n.name == body.name)) {
+        return res.status(400).json ({
+            error: 'name must be unique'
+        })
+    } else {
+        const person = {
+            name: body.name,
+            number: body.number,
+            id: generateId()
+        }
     }
 
     persons = persons.concat(person)
     res.json(person)
 })
 
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
