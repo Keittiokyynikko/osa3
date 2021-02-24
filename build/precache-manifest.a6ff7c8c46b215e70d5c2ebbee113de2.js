a66b158d215b03083ca835d78e09de8f1305a059
Keittiokyynikko <karvinen.juuso@gmail.com>
2021-02-24 17:37:24 +0200

    front

---
+++ build/precache-manifest.a6ff7c8c46b215e70d5c2ebbee113de2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d69c1227179637febe8d1cc0493a6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "306401d68c0e862fdc1f",
+    "url": "/static/css/main.f48d505c.chunk.css"
+  },
+  {
+    "revision": "f9aba95b3f951c008d68",
+    "url": "/static/js/2.c7511e6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7511e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306401d68c0e862fdc1f",
+    "url": "/static/js/main.ceec7c81.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
